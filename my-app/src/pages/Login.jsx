880ddc2a4092b33daaed0da0e33d03942747e9bd
Besiiiii <bh58330@ubt-uni.net>
2023-05-22 20:01:49 +0200

    log in

--- my-app/src/pages/Login.jsx
+++ my-app/src/pages/Login.jsx
@@ -1,16 +1,41 @@
-import { RiAccountCircleFill } from "react-icons/ri";
+import {RiAppleFill, RiFacebookCircleFill,} from "react-icons/ri";
 import { Link } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+import logo from "../images/logo.jpg";
 
 export const Login = () => {
     return (
         <div className="logindiv">
             <>
-        <form className="loginform">
+        
+        <img src={logo} alt="logo" className="logo" />
 
-                <div className="icona"><RiAccountCircleFill/></div>
-            <input type="text" placeholder="Username ..." />
-            <input type="password" placeholder="Password ..." />
-            <button type="submit"><Link to='/app' className="loginbutton">Login</Link></button>
+        <form className="loginform">
+            <h1 className="log">
+                Log In
+            </h1>
+            <input type="text" placeholder="Email..." />
+            <input type="password" placeholder="Password..." />
+            
+            <div className="rememforg">
+                <label className="remember">
+                    <input type="checkbox" className="checkboxi"/>
+                    Remember me </label>
+                <button className="forgot">Forgot Password</button>
+            </div>
+            
+            <button className="signin">Sign In</button>
+            <div className="under">
+                <h2>Log In via</h2>
+            <div className="iconat">
+                <RiFacebookCircleFill className="iconaf"/>
+                <FcGoogle className="icona"/>
+                <RiAppleFill className="icona"/>
+            </div>
+            <div className="creatediv">
+                <h5>Dosen't have an account?</h5><button className="create">Create Account</button>
+            </div>
+            </div>
         </form>
         </>
         </div>
